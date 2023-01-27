20fd7ece528e0d9fdc5b21bfa9c5f0b0c350e3ef
Ronaldo Alencar <alencarprincipal@gmail.com>
2023-01-27 00:27:10 -0300

    atualização 4 (lizard-spock)

--- script.js
+++ script.js
@@ -30,9 +30,24 @@ scissors[0].addEventListener("click", () => {
   generateComputerChoice();
   check("scissors");
 });
+//////////////////////////////////////////////////////////////
+const lizard = document.getElementsByClassName("lizard");
+lizard[0].addEventListener("click", () => {
+  lizard[0].classList.add("selected");
+  generateComputerChoice();
+  check("lizard");
+});
+//                                                                           ------------------>              Adicional lizard-spock
+const spock = document.getElementsByClassName("spock");
+spock[0].addEventListener("click", () => {
+  spock[0].classList.add("selected");
+  generateComputerChoice();
+  check("spock");
+});
+///////////////////////////////////////////////////////////////
 
 function generateComputerChoice() {
-  let possibleChoices = ["rock", "paper", "scissors"];
+  let possibleChoices = ["rock", "paper", "scissors", "lizard", "spock"]; ////////////////////////////////
   const randomChoice = Math.floor(Math.random() * possibleChoices.length);
   computerChoice = possibleChoices[randomChoice];
   computersImage[0].setAttribute(
@@ -42,7 +57,6 @@ function generateComputerChoice() {
 }
 
 // generateComputerChoice();
-// Falta dar valor às constantes
 
 // const rock = 0;
 // const scissors = 0;
@@ -58,17 +72,50 @@ function check(playerChoice) {
     result.innerText = draw; // se player 1 pedra e computador pedra, empate
   } else if (playerChoice === "rock" && computerChoice === "paper") {
     result.innerText = playerDefeat; // se player 1 pedra e computador tesoura, vitória computador
+  } else if (playerChoice === "rock" && computerChoice === "spock") {
+    result.innerText = playerDefeat; // /////////////////////////////           ------------------>              Adicional lizard-spock
   } else if (playerChoice === "rock" && computerChoice === "scissors") {
     result.innerText = playerVictory; // se player 1 pedra e computador tesoura, vitória player 1
+  } else if (playerChoice === "rock" && computerChoice === "lizard") {
+    result.innerText = playerVictory; // /////////////////////////////          ------------------>              Adicional lizard-spock
   } else if (playerChoice === "paper" && computerChoice === "rock") {
     result.innerText = playerVictory; // se player 1 papel e computador pedra, vitória player 1
+  } else if (playerChoice === "paper" && computerChoice === "spock") {
+    result.innerText = playerVictory; // /////////////////////////////          ------------------>              Adicional lizard-spock
   } else if (playerChoice === "paper" && computerChoice === "scissors") {
     result.innerText = playerDefeat; // se player 1 papel e computador tesoura, vitória computador
+  } else if (playerChoice === "paper" && computerChoice === "lizard") {
+    result.innerText = playerDefeat; // //////////////////////////////          ------------------>              Adicional lizard-spock
   } else if (playerChoice === "scissors" && computerChoice === "rock") {
     result.innerText = playerDefeat; // se player 1 tesoura e computador pedra, vitória computador
+  } else if (playerChoice === "scissors" && computerChoice === "spock") {
+    result.innerText = playerDefeat; // ///////////////////////////////         ------------------>              Adicional lizard-spock
   } else if (playerChoice === "scissors" && computerChoice === "paper") {
     result.innerText = playerVictory; // se player 1 tesoura e computador papel, vitória player 1
+  } else if (playerChoice === "scissors" && computerChoice === "lizard") {
+    result.innerText = playerVictory; // //////////////////////////////
+  } else if (playerChoice === "lizard" && computerChoice === "paper") {
+    result.innerText = playerVictory; // //////////////////////////////         ------------------>
+  } else if (playerChoice === "lizard" && computerChoice === "spock") {
+    result.innerText = playerVictory; // /////////////////////////////          ------------------>
+  } else if (playerChoice === "lizard" && computerChoice === "scissors") {
+    result.innerText = playerDefeat; // //////////////////////////////          ------------------>
+  } else if (playerChoice === "lizard" && computerChoice === "rock") {
+    result.innerText = playerDefeat; // /////////////////////////////           ------------------>              Adicional lizard-spock
+  } else if (playerChoice === "spock" && computerChoice === "paper") {
+    result.innerText = playerDefeat; // /////////////////////////////           ------------------>
+  } else if (playerChoice === "spock" && computerChoice === "rock") {
+    result.innerText = playerVictory; // /////////////////////////////          ------------------>
+  } else if (playerChoice === "spock" && computerChoice === "scissors") {
+    result.innerText = playerVictory; // /////////////////////////////          ------------------>
+  } else if (playerChoice === "spock" && computerChoice === "lizard") {
+    result.innerText = playerDefeat; // /////////////////////////////           ------------------>
   }
 }
+
+// -----------------------------------------------------------------
+
+//
+
 // Ctrl + K + C para comentar
 // Ctrl + K + U para remover comentário
